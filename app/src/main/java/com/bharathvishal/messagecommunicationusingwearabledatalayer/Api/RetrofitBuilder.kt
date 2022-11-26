7a1667123c91c1b9ee40367e13dccf012b4d097f
the-groot <sangwoo0795@naver.com>
2022-11-26 16:05:32 +0900

    챗봇 기능 옮기는중(1)

---
+++ app/src/main/java/com/bharathvishal/messagecommunicationusingwearabledatalayer/Api/RetrofitBuilder.kt
@@ -0,0 +1,9 @@
+package com.bharathvishal.messagecommunicationusingwearabledatalayer.Api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+
+interface RetrofitBuilder {
+}
